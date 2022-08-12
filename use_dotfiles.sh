66e1c6f0ff1f2ba3f3e2abee4d6ec42bfc9f9745
Kasper Sauramo <k474s@protonmail.ch>
2022-08-12 18:38:51 +0300

    add some wip dotfiles

--- use_dotfiles.sh
+++ use_dotfiles.sh
@@ -1,5 +1,27 @@
 #!/usr/bin/bash
 
+# i: ask override if exists
+
 # Neovim
-mkdir ~/.config/nvim/
+mkdir -p ~/.config/nvim/
 ln -si $(pwd)/nvim/init.vim ~/.config/nvim/init.vim
+
+# Zsh
+ln -si $(pwd)/zsh/.zshrc ~/.zshrc
+
+# Xresources
+ln -si $(pwd)/Xresources/.Xresources ~/.Xresources
+
+# i3
+ln -si $(pwd)/i3/config ~/.config/i3/config
+
+# alacritty
+mkdir -p ~/.config/alacritty/
+ln -si $(pwd)/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+
+# polybar
+mkdir -p ~/.config/polybar/
+ln -si $(pwd)/polybar/config.ini ~/.config/polybar/config.ini
+
+# redshift
+ln -si $(pwd)/redshift/redshift.conf ~/.config/redshift.conf
