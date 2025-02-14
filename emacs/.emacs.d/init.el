a8625950699dfc9460ab627ad096b2c9e9d0f16e
Kasper Sauramo <kasper.sauramo@analogway.com>
2025-02-14 21:32:57 +0200

    Remove .emacs in favor if init.el

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,3 +1,19 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(deeper-blue))
+ '(inhibit-startup-screen t)
+ '(package-selected-packages '(## markdown-mode))
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
 
 (require 'package)
 (add-to-list 'package-archives
