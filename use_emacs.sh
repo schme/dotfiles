0ad3aacf2ac5b212358fb380142d7f26c6044cd6
Kasper Sauramo <kasper.sauramo@analogway.com>
2025-02-14 21:45:10 +0200

    add emacs init file

---
+++ use_emacs.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+sudo apt install emacs
+
+mkdir -p ~/.emacs.d/
+ln -sf "$(pwd)/emacs/.emacs.d/init.el" ~/.emacs.d/init.el
+
+git clone --depth 1 --single-branch https://github.com/doomemacs/doomemacs ~/.config/emacs
+~/.config/emacs/bin/doom install
