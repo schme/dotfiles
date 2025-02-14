67ee0fa434870e5c46fe56eedb49bc310caa5b73
Kasper Sauramo <kasper.sauramo@analogway.com>
2025-02-14 21:51:54 +0200

    add emacs docs to install on debian

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -3,7 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(deeper-blue))
+
+ '(custom-safe-themes
+   '("02f57ef0a20b7f61adce51445b68b2a7e832648ce2e7efb19d217b6454c1b644" default))
  '(inhibit-startup-screen t)
  '(package-selected-packages '(## markdown-mode))
  '(tool-bar-mode nil))
--- use_emacs.sh
+++ use_emacs.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 sudo apt install emacs
+sudo apt install emacs-common-non-dfsg
 
 mkdir -p ~/.emacs.d/
 ln -sf "$(pwd)/emacs/.emacs.d/init.el" ~/.emacs.d/init.el
