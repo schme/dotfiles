93a56d10bfbe63433930161f9d1c54fe925677b7
Kasper Sauramo <kasper.sauramo@analogway.com>
2025-02-16 13:08:05 +0200

    remove vanilla emacs config link from use_emacs

--- use_emacs.sh
+++ use_emacs.sh
@@ -3,8 +3,5 @@
 sudo apt install emacs
 sudo apt install emacs-common-non-dfsg
 
-mkdir -p ~/.emacs.d/
-ln -sf "$(pwd)/emacs/.emacs.d/init.el" ~/.emacs.d/init.el
-
 git clone --depth 1 --single-branch https://github.com/doomemacs/doomemacs ~/.config/emacs
 ~/.config/emacs/bin/doom install
