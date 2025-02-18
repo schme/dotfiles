b60889163c0e310ea73e040097414d99fa2ab0ed
Kasper Sauramo <kasper.sauramo@analogway.com>
2025-02-18 08:57:08 +0200

    add doom stuff

--- use_emacs.sh
+++ use_emacs.sh
@@ -5,3 +5,5 @@ sudo apt install emacs-common-non-dfsg
 
 git clone --depth 1 --single-branch https://github.com/doomemacs/doomemacs ~/.config/emacs
 ~/.config/emacs/bin/doom install
+
+ln -si "$(pwd)/emacs/doom" ~/.config/doom
--- use_zsh.sh
+++ use_zsh.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-script_dir=dirname "$0"
-echo "Target file: $script_dir/zsh/.zshrc"
+script_dir="dirname $0"
+echo Target file: "$script_dir/zsh/.zshrc"
 
 # Zsh
 mv ~/.zshrc ~/.zshrc.bu
